3d838a8ff13b35fee550515005c084629e55b998
Salvador Martinez Espinosa <84885134+salvadormartinez50@users.noreply.github.com>
2021-12-26 22:28:38 -0600

    GET

--- src/main/java/sincronizar/chava/RecursoDeSaludo.java
+++ src/main/java/sincronizar/chava/RecursoDeSaludo.java
@@ -3,14 +3,20 @@ package sincronizar.chava;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import sincronizar.models.Hola;
+
 
 @Path("/hola")
 public class RecursoDeSaludo {
+	
 
 	@GET
 	@Produces("application/json")
-	public String Hola() {
-		return "Hola Servicio REST";
+	public Hola Hola() {
+		Hola hola = new Hola();
+		return hola;
 	}
 
 }
